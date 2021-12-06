1b17f3b6183c75016aa199a075fac43aef1373c3
andev0 <fejalex@gmail.com>
2021-12-06 12:26:07 +0200

    Moved all game logic from main to Game class

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,30 +1,13 @@
 #include <iostream>
 #include <string>
 
-#include <fmt/format.h>
-#include <fmt/color.h>
-
-//#include <windows.h>
-
-#define GREEN_COLOR std::string("\e[1;32m")
-#define RED_COLOR std::string("\e[1;31m")
-#define RETURN_COLOR std::string("\e[0m")
-
-#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
-#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x4
-#endif
+#include "Base/Game.h"
 
 int main()
 {
-    // SetConsoleCP(65001);
+    fw::App* app = new fw::Game();
 
-    // HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    // DWORD dwMode = 0;
-    // GetConsoleMode(hOut, &dwMode);
-    // dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-    // SetConsoleMode(hOut, dwMode);
-
-    fmt::print(fmt::format(RED_COLOR + "This is {0} language.\n" + RETURN_COLOR, "русский"));
+    app->start();
 
     return 0; 
 }
